d0c92abb1996fca37803e569969841d7deb04761
Danil0-1 <danilomuskus1@gmail.com>
2025-07-17 23:35:04 -0500

    feat: :lipstick: Mas proyectos agregados , cambios en los colores de los botones

--- index.js
+++ index.js
@@ -12,10 +12,34 @@ const projects = [
     link: "https://github.com/NicolasMuskusTarazona/Formula-1"
   },
   {
-    title: "Cocina de Maryann",
+    title: "Cocina",
     description: "Landing page responsive con animaciones y presentación de platos.",
     image: "docs/Img/maryann.jpg",
     link: "https://danil0-1.github.io/Maryann-Food/Informacion.html"
+  },
+  {
+    title: "App de ecommerce de ropa",
+    description: "Aplicación de ropa con pantallas animadas. Solo se ve bien en celular (modo vertical).",
+    image: "docs/Img/ecommerce.jpg",
+    link: "https://danil0-1.github.io/app_de_ecommerce_de_ropa/views/splashscreen.html"
+  },
+  {
+    title: "50 Landings Creativas",
+    description: "Diseños modernos de landing page se adapta solo en celular",
+    image: "docs/Img/50landings.jpg",
+    link: "https://danil0-1.github.io/50---Landing-page-designs--Community-/"
+  },
+  {
+    title: "Sitio accesible para personas con discapacidad visual",
+    description: "Prototipo con alto contraste y diseño pensado para escritorio. Solo se ve bien en PC.",
+    image: "docs/Img/discapacidad.png",
+    link: "https://danil0-1.github.io/Prototipo-interactivo-de-p-gina-web-para-personas-con-dificultades-visuales--Community-/views/black-home.html"
+  },
+  {
+    title: "Calculadora práctica",
+    description: "Calculadora interactiva con operaciones básicas. Proyecto personal con animaciones suaves.",
+    image: "docs/Img/Calculadora.jpg", 
+    link: "https://danil0-1.github.io/Calculadora-Danilo-Practice/"
   }
 ];
 
@@ -24,10 +48,12 @@ const container = document.getElementById("projectCards");
 projects.forEach((project, index) => {
   const card = document.createElement("div");
   card.className = "card animate-entry";
-  card.style.animationDelay = `${index * 0.2}s`; 
+  card.style.animationDelay = `${index * 0.2}s`;
 
   card.innerHTML = `
-    <img src="${project.image}" alt="${project.title}">
+    <a href="${project.link}" target="_blank">
+      <img src="${project.image}" alt="${project.title}">
+    </a>
     <h3>${project.title}</h3>
     <p>${project.description}</p>
     <a href="${project.link}" target="_blank">Ver proyecto →</a>
