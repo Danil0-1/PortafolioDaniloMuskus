1368a61a0eec94864f02d6949cd246ee1d4d2283
Danil0-1 <danilomuskus1@gmail.com>
2025-07-19 11:07:39 -0500

    feat: :lipstick: Cambios para que la animacion del titulo funcione

--- docs/commponents/animacionTitulo.js
+++ docs/commponents/animacionTitulo.js
@@ -1,25 +1,11 @@
-const texto = "Desarrollador Web Junior | HTML, CSS, JavaScript";
-let i = 0;
-let escribiendo = true;
+const baseTitle = "   Danilo Muskus Tarazona | Portafolio   "; 
+let position = 0;
 
-function efectoMaquina() {
-  const titulo = document.getElementById("typing-title");
-
-  if (escribiendo) {
-    titulo.textContent = texto.substring(0, i++);
-    if (i > texto.length) {
-      escribiendo = false;
-      setTimeout(efectoMaquina, 1500); 
-      return;
-    }
-  } else {
-    titulo.textContent = texto.substring(0, i--);
-    if (i < 0) {
-      escribiendo = true;
-    }
-  }
-
-  setTimeout(efectoMaquina, 60);
+function animateTitle() {
+  const scrollText = baseTitle.substring(position) + baseTitle.substring(0, position);
+  document.title = scrollText;
+  position = (position + 1) % baseTitle.length;
+  setTimeout(animateTitle, 100);
 }
 
-window.addEventListener("load", efectoMaquina);
+animateTitle();
